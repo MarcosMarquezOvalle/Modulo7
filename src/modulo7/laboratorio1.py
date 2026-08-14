30e3c9c99710852c86d2368872f71933148d13b1
MarcosMarquezOvalle <mmarquez17@gmail.com>
2026-08-14 11:54:18 -0600

    Fix the indentation of the code block to ensure proper execution and readability.

--- src/modulo7/laboratorio1.py
+++ src/modulo7/laboratorio1.py
@@ -1,5 +1,8 @@
+from __future__ import annotations
+
 import httpx
-from httpx_retries import Retry, RetryTransport
+from httpx_retries import Retry
+from httpx_retries import RetryTransport
 
 timeout = httpx.Timeout(connect=10.0, read=30.0, write=30.0, pool=5.0)
 
@@ -10,12 +13,12 @@ url = "https://api.github.com"
 file_path = "output_file.txt"
 
 with httpx.Client(timeout=timeout, transport=retry_transport) as client:
-  with client.stream("GET", url) as response:
-    response.raise_for_status()
-    with open(file_path, "wb") as f:
-      for chunk in response.iter_bytes(chunk_size=1000):
-        print(chunk)
-        print(f"Downloaded {len(chunk)} bytes")
-        f.write(chunk)
+    with client.stream("GET", url) as response:
+        response.raise_for_status()
+        with open(file_path, "wb") as f:
+            for chunk in response.iter_bytes(chunk_size=1000):
+                print(chunk)
+                print(f"Downloaded {len(chunk)} bytes")
+                f.write(chunk)
 
 print(f"Downloaded successfully to {file_path}")
